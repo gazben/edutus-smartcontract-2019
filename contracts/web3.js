b4cdcdd54cdfbc4c0b21b40497aadd88de0d1b01
Gazder Bence <bencegazder@gmail.com>
2019-12-07 11:53:27 +0100

    Add web3 init

--- contracts/web3.js
+++ contracts/web3.js
@@ -1,6 +1,15 @@
-// import web3 from 'web3'
+import Web3 from 'web3'
 
 if(!window.ethereum) {
     alert('Nem található a metamask kiegészítő. Kérlek telepítsd fel!')
 }
 
+window.web3 = new Web3(window.ethereum);
+
+try {
+    window.ethereum.enable()
+    console.log('Metamask init success!')
+}catch (e) {
+    console.error('Metamask init failed')
+    console.log(e)
+}
