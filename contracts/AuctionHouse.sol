e9c660c4245a2fb6e55a9dc1339ac0dcdc140303
Gazder Bence <bencegazder@gmail.com>
2019-12-07 12:56:16 +0100

    Add initial AuctionHouse contract version

---
+++ contracts/AuctionHouse.sol
@@ -0,0 +1,68 @@
+pragma solidity >0.5.5;
+
+contract AuctionHouse {
+    uint256 auctionCount = 0;
+    Auction[] public auctions;
+
+    function createAuction(
+        string memory _title,
+        string memory _description
+        ) public {
+          Auction newAuction = new Auction(msg.sender, _title, _description);
+          auctions.push(newAuction);
+          auctionCount++;
+        }
+}
+
+contract Auction {
+    string title;
+    string description;
+    address payable owner;
+    uint256 highestBid;
+    address payable highestBidOwner;
+
+    enum State{Listed, Started, Finished}
+    State state;
+
+    constructor(
+        address payable _owner,
+        string memory _title,
+        string memory _description
+        ) public {
+            owner = _owner;
+            title = _title;
+            description = _description;
+            highestBid = 0;
+            state = State.Listed;
+    }
+
+    function start() public {
+        require(state == State.Listed, "Auction state has to be Listed!");
+        require(owner == msg.sender, "Only the owner can start the auction!");
+        state = State.Started;
+    }
+
+    function close() public {
+        require(state == State.Started, "The auction has to be started!");
+        require(owner == msg.sender, "Only the owner can close the auction!");
+        // TODO: time expired or max bid reached
+        state = State.Finished;
+        owner = highestBidOwner;
+    }
+
+    function bid() public payable {
+        require(msg.value > highestBid, "Your bid needs to be higher than the current!");
+        require(msg.sender != highestBidOwner, "YOu cannot bid on top of your own!");
+
+        highestBidOwner.transfer(highestBid);
+
+        highestBid = msg.value;
+        highestBidOwner = msg.sender;
+    }
+
+    function finalBid() public view returns(uint256) {
+        require(state == State.Finished, "Auction needs to be in Finished state!");
+
+        return highestBid;
+    }
+}
